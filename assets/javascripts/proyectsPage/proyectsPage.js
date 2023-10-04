c1550aae911a638086cb076fa93b725cdc9e57df
Luciano Cardozo Casariego <lucianoc.c117@gmail.com>
2023-10-04 19:26:08 -0300

    Proyect page WIP

--- assets/javascripts/proyectsPage/proyectsPage.js
+++ assets/javascripts/proyectsPage/proyectsPage.js
@@ -1,2 +1,7 @@
 const proyectID = localStorage.getItem("proyect_id") || 0;
-console.log(proyectList[proyectID]);
+//console.log(proyectList[proyectID]);
+
+//TODO hacer la parte del abstract en descktop
+//TODO mover los estilos que correspondan a las variables
+//TODO mover el abstract al JS para buscar la info del JSON
+//TODO seguir con el contenido
